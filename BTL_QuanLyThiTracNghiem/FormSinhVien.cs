94eed98c1f927f819b8599c359a9e7c051c84ddf
Huy dzz <huymin96@gmail.com>
2019-04-04 15:38:51 +0700

    Initial Project

--- BTL_QuanLyThiTracNghiem/FormSinhVien.cs
+++ BTL_QuanLyThiTracNghiem/FormSinhVien.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 
+//lan1
+
 namespace BTL_QuanLyThiTracNghiem
 {
     public partial class FormSinhVien : Form
